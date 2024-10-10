ada8d121f73aff892473e3656d8332b761fb6097
camilafagundes <fagundescamilaferraz@gmail.com>
2024-10-10 20:11:33 -0300

    Calculadora corrigida

---
+++ index.js
@@ -0,0 +1,29 @@
+let vitorias = 85; 
+let derrotas = 30;
+
+function calcularNivel(vitorias, derrotas) {
+    const saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (saldoVitorias < 10) {
+        nivel = "Ferro";
+    } else if (saldoVitorias >= 11 && saldoVitorias <= 20) {
+        nivel = "Bronze";
+    } else if (saldoVitorias >= 21 && saldoVitorias <= 50) {
+        nivel = "Prata";
+    } else if (saldoVitorias >= 51 && saldoVitorias <= 80) {
+        nivel = "Ouro";
+    } else if (saldoVitorias >= 81 && saldoVitorias <= 90) {
+        nivel = "Diamante";
+    } else if (saldoVitorias >= 91 && saldoVitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return `O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`;
+}
+
+// Exemplo de uso
+const resultado = calcularNivel(85, 30);
+console.log(resultado);
